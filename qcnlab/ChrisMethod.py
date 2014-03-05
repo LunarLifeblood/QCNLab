646097d4062e9cd73ab86feccb71daf8e7a2023c
Chris Brain <lunarhawk@hotmail.co.uk>
2014-03-05 22:44:59 +0000

    started trying to find the levels in the data

--- qcnlab/ChrisMethod.py
+++ qcnlab/ChrisMethod.py
@@ -14,10 +14,15 @@ def getValues(numberBins, voltReading, eSqHVal):
     volts = voltReading
     eSqH = eSqHVal
 
-def writeDiffData(data):
+def writeDiffData(data, magnitude):
     fw = open("diffdata.csv", "w")
+    magnitude = 0.01* magnitude
     for i, value in enumerate(data):
-        fw.write(str(i)+","+str(value)+"\n")
+        if i != 0:
+            if abs(value)<magnitude:
+                fw.write(str(i)+","+str(value)+",0.5\n")
+            else:
+                fw.write(str(i)+","+str(value)+",0\n")
     fw.close()
     
 def formHistogram(outputFile, listOfDirs):
@@ -39,11 +44,23 @@ def formHistogram(outputFile, listOfDirs):
             maximum = 0
             minimum = 0
             for line in fs:
-                cell = float(line.split(","))
-                
+                cell = line.split(",")
+                cell[4] = float(cell[4])
+                if cell[4] > maximum:
+                    maximum = cell[4]
+                elif cell[4] < minimum:
+                    minimum = cell[4]
                 data.append(cell[4]- preCell)
                 preCell = float(cell[4])
-            writeDiffData(data)
+            if abs(maximum) > abs(minimum):
+                if(abs(maximum) < 1.67):
+                    writeDiffData(data, abs(maximum))
+            elif abs(maximum) < abs(minimum):
+                writeDiffData(data, abs(minimum))
+            else:
+                print("UH OH!")
             
+            
+    print("finished")
     
     
