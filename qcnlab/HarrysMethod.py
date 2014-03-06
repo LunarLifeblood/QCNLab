75aebe2efedc768d0bb2e219fd300c8cd90a1314
HStromfelt <harrystromfelt@hotmail.co.uk>
2014-03-06 23:29:53 +0000

    I have edited the step analysis. Now we look for a top and then search on for a bottom. Once this is done, we carry on from the bottom and look for a new top. We also filter out any steps that are smaller than our lowest expected, and then output the data points between which we got the mini step.
    Must add the directory too so we know which file set to look at...?

--- qcnlab/HarrysMethod.py
+++ qcnlab/HarrysMethod.py
@@ -18,6 +18,8 @@ def formHistogram(outputFile, listOfDirs):
     valueList = []
     stepListVoltageValues = []
     fs = None
+    maximum=0
+    minimum=0
     global numBins, volts, eSqH, binSize, listOfBins
     listOfBins = functions.createZeroedList(numBins)
     for directory in listOfDirs:
@@ -31,23 +33,29 @@ def formHistogram(outputFile, listOfDirs):
         if success == True:
             for line in fs:
                 cell = line.split(",")
-                functions.convert(valueList.append(cell[4]))
-            for i in range (0, len(valueList), 1):
-                if((valueList[i+2]-valueList[i+1])>((valueList[i+1]-valueList[i]))):
-                    Top = valueList[i+1]
-                elif(((valueList[i+2]-valueList[i+1])<=(valueList[i+1]-valueList[i]))-(20*10e-3)):
-                    Bottom = valueList[i+2]
-                if((functions.convert(Top-Bottom) >= eSqH)):
-                    stepListVoltageValues.append(Top-Bottom)
-                    minimum = min(stepListVoltageValues)
-                    maximum = max(stepListVoltageValues)
-                else:
-                    print("I love Chris")
-            binSize = (maximum - minimum)/(numBins-1)
-            for i in range (0, len(stepListVoltageValues), 1):
-                listOfBins[int(stepListVoltageValues[i]/numBins)] +=1
+                valueList.append(functions.convert(cell[4]),volts,eSqH)
+            while(True):
                 
-    fs = open(outputFile, "w")
+                for i in range (0, len(valueList), 1):#Start loop for at top value search
+                    if((valueList[i+2]-valueList[i+1])>((valueList[i+1]-valueList[i]))):
+                        Top = valueList[i+1]
+                        j=i+1 #store index for last value assessed
+                        for z in range (j, len(valueList), 1): #now carry on only looking for a much smaller increment size to give bottom of step
+                            if (((valueList[z+2]-valueList[z+1])<=(valueList[z+1]-valueList[z]))-(35*10e-3)):
+                                Bottom = valueList[z+2]
+                                i=z+2 #store last index assessed
+                                break #break out of loop so we can calc the step
+                    if((functions.convert(Top-Bottom) >= eSqH)): #filtering out the accidental mini steps
+                        stepListVoltageValues.append(Top-Bottom)
+                        minimum = min(stepListVoltageValues) #analyzing for the max and min range of step sizes,
+                        maximum = max(stepListVoltageValues)
+                    else:
+                        print("false step between " + str(j) +"-" + str(i)) #explaining where we miscalc a step to then assess for debug
+        binSize = (maximum - minimum)/(numBins-1) #To be done after data set has been full analyzed, so once while loop is ended
+        for i in range (0, len(stepListVoltageValues), 1):
+            listOfBins[int(stepListVoltageValues[i]/numBins)] +=1
+                
+    fs = open(outputFile, "w") #make the histo
     halfBin = binSize/2
     for i, item in enumerate(listOfBins):
         fs.write((str((i*binSize)+halfBin))+","+str(item)+"\n")
