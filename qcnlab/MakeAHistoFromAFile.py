99ca8962269347040ad081ea2c95a038b9a40d90
Chris Brain <lunarhawk@hotmail.co.uk>
2014-03-11 14:51:18 +0000

    fixed errors in make a histo file

--- qcnlab/MakeAHistoFromAFile.py
+++ qcnlab/MakeAHistoFromAFile.py
@@ -11,11 +11,11 @@ for directory in listOfDirs:
     fs.close()
     minimum = min(data)
     maximum = max(data)
-    binSize = (maximum-minimum)/numBins
+    binSize = (maximum-minimum)/(numBins-1)
     for item in data:
-        listOfBins[int(item/binSize)] += 1
+        listOfBins[int((item+abs(minimum))/binSize)] += 1
     
 fs = open("somefile.csv", "w")
 halfBin = binSize/2
 for i, item in enumerate(listOfBins):
-    fs.write(str((i*binSize) +halfBin)+","+str(item)+"\n")
+    fs.write(str((i*binSize) +halfBin-abs(minimum))+","+str(item)+"\n")
